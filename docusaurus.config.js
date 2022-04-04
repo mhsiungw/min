ade30dedf55ac96850670c777484449de95b93d1
minh_w <minhsiungw@gmail.com>
2022-04-04 10:06:21 +0800

    change url and editUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 /** @type {import('@docusaurus/types').Config} */
 const config = {
     title: 'min',
-    url: 'https://your-docusaurus-test-site.com',
+    url: 'https://minhsiungw.github.io',
     baseUrl: '/',
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
@@ -25,16 +25,14 @@ const config = {
                     routeBasePath: '/',
                     sidebarPath: require.resolve('./sidebars.js'),
                     // Please change this to your repo.
-                    editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                    editUrl: 'https://github.com/mhsiungw/minhsiungw.github.io',
                 },
                 blog: {
                     path: 'portfolio',
                     routeBasePath: 'portfolio',
                     showReadingTime: true,
                     // Please change this to your repo.
-                    editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                    editUrl: 'https://github.com/mhsiungw/minhsiungw.github.io',
                 },
                 theme: {
                     customCss: require.resolve('./src/css/custom.css'),
